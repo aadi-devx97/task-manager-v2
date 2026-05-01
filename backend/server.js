7ca6924bc728c34a1a9fcfa51a76efb2000a586c
Aadi <aadijain.ltd97@gmail.com>
2026-05-01 11:31:50 +0530

    Fix port for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = proccess.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
